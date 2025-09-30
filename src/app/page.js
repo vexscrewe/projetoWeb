6490785b66438d79727d3af909a647e82793171e
vexscrewe <monokumaphu@gmail.com>
2025-09-30 19:35:07 -0400

    ConfigMongo

--- src/app/page.js
+++ src/app/page.js
@@ -28,7 +28,7 @@ export default function Page() {
         <form onSubmit={salvarDados}>
             <input type = "text" placeholder="Digite Nome:" onChange = {(event)=> setNome(event.target.value)}/>
         
-            <input type = "number" placeholder="Digite matricula" onChange={(event)=> setMatricula(event.target.value)}/>
+            <input type = "number" placeholder="Digite matricula:" onChange={(event)=> setMatricula(event.target.value)}/>
             <input type = "text" placeholder="Digite curso" onChange={(event)=> setCurso(event.target.value)}/>
           <button>Salvar</button>
         </form>
